aa2627e3b0cbd442fc6691d51f91df92170eb64c
Vinícius Garcia <vingarcia00@gmail.com>
2019-03-06 00:38:01 -0300

    Add Python script for simulating battles

---
+++ scripts/combat.py
@@ -0,0 +1,116 @@
+
+import random
+
+def fight(*teams):
+    players = {}
+    for team in teams:
+        oponents = [p for t in teams if t != team for p in t]
+        for char in team:
+            players[char.name] = {
+                'char': char,
+                'team': team,
+                'initiative': random.randint(1, 20) + char.modif('agi'),
+                'oponents': oponents,
+            }
+
+    commandList = {
+        'hit': hitCmd,
+        'auto': autoCmd,
+        'overview': overviewCmd,
+        'run': runCmd,
+        '': autoCmd,
+    }
+
+    order = sorted(players.values(), key=lambda x: x['initiative'])
+    while True:
+        for player in order:
+            player['char'].restore('balance')
+            print('%s`s turn!\n' % player['char'].name)
+            print('Player:', player['char'])
+
+            while True:
+                cmd = ['auto']
+                if player['char'].isPlayer:
+                    cmd = input(
+                        '\nCommands:\n' +
+                        '  hit [Name]\n' +
+                        '  overview\n' +
+                        '  run\n\n'
+                    )
+                    cmd = cmd.split(' ')
+
+                if cmd[0] in commandList:
+                    action = commandList[cmd[0]]
+                    try:
+                        result = action(player, players, teams, cmd[1:])
+                        if result == 'retry':
+                            continue
+                        if result == 'exit':
+                            return
+                        break
+                    except Exception as e:
+                        print(e)
+
+            if fightIsOver(teams):
+                return
+
+            input('Hit enter for next turn...\n')
+
+def fightIsOver(teams):
+    teamsAlive = 0
+    for team in teams:
+        for char in team:
+            if not char.stats['disabled']:
+                teamsAlive += 1
+                break
+
+    return teamsAlive == 1
+
+##
+# Actions:
+#
+
+def autoCmd(player, players, teams, args):
+    oponent = random.choice(
+        [op for op in player['oponents'] if not op.stats['disabled']]
+    )
+
+    hitCmd(player, players, teams, [oponent.name])
+
+    return 'ok'
+
+
+def hitCmd(player, players, teams, args):
+    if len(args) < 1 or type(args[0]) != str or args[0] not in players:
+        raise Exception('Invalid or missing target name for `hit` command: %s' % args)
+    oponent = players[args[0]]['char']
+    print('Oponent chosen:', oponent)
+
+    attack = player['char'].attack()
+    print('\nAttack:', attack)
+    if attack['hit_chance'] >= 12:
+        result = oponent.defend(attack['dmg'])
+        if type(result) != int:
+            print('Successful hit but it was', result)
+        else:
+            print('Successful hit of', result)
+        if oponent.stats['disabled']:
+            print(oponent.name, 'is DOWN!')
+    else:
+        print('Missed!')
+    print()
+
+    return 'ok'
+
+def overviewCmd(player, _players, teams, _args):
+    print('\nTeams:')
+    for team in teams:
+        print(team)
+    print()
+
+    return 'retry'
+
+def runCmd(player, _players, teams, _args):
+    list(teams).remove(player['team'])
+
+    return 'exit'
---
+++ scripts/domain.py
@@ -0,0 +1,136 @@
+
+import random
+import json
+
+from combat import fight
+
+class Character:
+    attrNames = [
+        'dex',
+        'agi',
+        'int',
+        'vit',
+        'str',
+    ]
+
+    def __init__(self, name, isPlayer=False, weapons=[], armor=0, **kw):
+        self.name = name
+        self.isPlayer = isPlayer
+        self.attrs = Character.distributeAttributes(kw, 25)
+        self.weapons = [2+2*x for x in (weapons or [1]) if x in range(1, 5)]
+        self.armor = armor
+        self.stats = self.defaultStats()
+
+    def attack(self):
+        if self.stats['disabled']:
+            return { 'hit_chance': 0, 'dmg': 0 }
+
+        hit = random.randint(1,20)
+        dmg = 0
+        for maxDmg in self.weapons:
+            dmg += random.randint(1, maxDmg)
+
+        return { 'hit_chance': hit, 'dmg': dmg }
+
+    def defend(self, dmg, dodge=True):
+        if dodge:
+            if dmg <= self.stats['balance']:
+                self.stats['balance'] -= dmg
+                return 'dodged'
+
+            dmg -= self.stats['balance']
+            self.stats['balance'] = 0
+
+        dmg -= self.armor
+        if dmg <= 0:
+            return 'blocked'
+
+        self.stats['hp'] -= dmg
+
+        self.stats['disabled'] = self.stats['hp'] <= 0
+        self.stats['dead'] = self.stats['hp'] <= -self.attrs['vit']
+
+        return dmg
+
+    def dispute(self, attrName):
+        offset = self.modif(attrName)
+        return random.randint(1, 8) + offset
+
+    def restore(self, stat='all'):
+        if stat == 'all':
+            self.stats = self.defaultStats()
+        else:
+            self.stats[stat] = self.defaultStats()[stat]
+
+    def defaultStats(self):
+        return {
+            'balance': self.attrs['agi'],
+            'mana': self.attrs['int'],
+            'hp': self.attrs['vit'],
+            'disabled': False,
+            'dead': False,
+        }
+
+    def modif(self, attrName, avgPts=5):
+        return self.attrs[attrName]-avgPts
+
+    @staticmethod
+    def distributeAttributes(baseAttrs, points, minPts=3, maxPts=8):
+        attrs = {k: baseAttrs[k] for k in Character.attrNames if k in baseAttrs}
+        missing = [x for x in Character.attrNames if x not in baseAttrs]
+
+        for k in attrs:
+            points -= attrs[k]
+
+        if points < minPts*len(missing):
+            raise Exception('Not enough points to produce a valid character!')
+
+        if points > maxPts*len(missing):
+            raise Exception('Too much points to produce a valid character!')
+
+        for k in missing:
+            attrs[k] = minPts
+            points -= minPts
+
+        while points > 0:
+            key = random.choice(missing)
+            attrs[key] += 1
+            points -= 1
+            if attrs[key] == maxPts:
+                missing.remove(key)
+
+        return attrs
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        weaponsStr = {
+            4: '1d4',
+            6: '1d6',
+            8: '1d8',
+            10: '1d10',
+            12: '1d12',
+        }
+
+        return '{\n  "name": "%s",\n  "attrs": %s,\n  "weapons": %s\n  "stats": %s\n}' % (
+            self.name,
+            self.attrs,
+            [weaponsStr[w] for w in self.weapons],
+            self.stats,
+        )
+
+monster = Character(
+    name='Zim',
+    weapons=[1, 2],
+)
+# print('Monster:', monster)
+
+player = Character(
+    name='Fox',
+    weapons=[2, 2],
+    isPlayer=True,
+)
+# print('Player:', player)
+
+fight([player], [monster])
