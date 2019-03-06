7dc6feb3672e979d1643b59622a13f9a8ccdac09
Vinícius Garcia <vingarcia00@gmail.com>
2019-03-06 01:32:04 -0300

    Improve readability of main.py file

--- scripts/main.py
+++ scripts/main.py
@@ -9,6 +9,6 @@ with open('sample.json') as file:
     players = json.load(file)
 
 players = [Character(**p) for p in players]
-teams = [list(t[1]) for t in groupby(players, key=lambda p: p.team)]
+teams = [list(members) for name, members in groupby(players, key=lambda p: p.team)]
 
 fight(*teams)
