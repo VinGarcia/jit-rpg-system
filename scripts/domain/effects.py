a1ef84da0df661a9371df23a623daf2b51304e89
Vinícius Garcia <vingarcia00@gmail.com>
2019-03-07 23:54:09 -0300

    Add first version of the Effects base class

---
+++ scripts/domain/effects.py
@@ -0,0 +1,10 @@
+
+class Effects:
+    def attack(self, player, dices):
+        pass
+
+    def defend(self, player, dmg, dodge):
+        pass
+
+    def dispute(self, player, attrName):
+        pass
